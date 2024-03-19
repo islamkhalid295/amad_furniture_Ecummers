7433eb496c6b8651d7c4c695df9ed1b883850737
Islam Khalid <islamkhalid295@gmail.com>
2024-03-19 22:13:17 +0200

    .

---
+++ lib/features/cart_screen/presentation/manager/product_cubit.dart
@@ -0,0 +1,12 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'product_state.dart';
+
+class ProductCubit extends Cubit<ProductState> {
+  ProductCubit() : super(ProductInitial());
+
+  void emitLoading(){
+    emit(Loading());
+  }
+}
---
+++ lib/features/cart_screen/presentation/manager/product_state.dart
@@ -0,0 +1,7 @@
+part of 'product_cubit.dart';
+
+@immutable
+abstract class ProductState {}
+
+class ProductInitial extends ProductState {}
+class Loading extends ProductState {}
